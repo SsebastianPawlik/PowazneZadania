7e43d2b74099637a1e149b827a085b866bf4d629
SsebastianPawlik <pawlik.sebastian00@gmail.com>
2022-08-04 14:43:31 +0200

    juz koncowka

--- 2.KompresjaSzyfrowanie/Form1.Designer.cs
+++ 2.KompresjaSzyfrowanie/Form1.Designer.cs
@@ -34,11 +34,17 @@
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.Dekompresja = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Plik = new System.Windows.Forms.TextBox();
+            this.WybierzPlik = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Location = new System.Drawing.Point(11, 69);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -49,17 +55,18 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(13, 43);
+            this.checkBox1.Location = new System.Drawing.Point(12, 130);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(150, 17);
             this.checkBox1.TabIndex = 1;
             this.checkBox1.Text = "Szyfrowanie asymetryczne";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(12, 66);
+            this.checkBox2.Location = new System.Drawing.Point(11, 153);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(144, 17);
             this.checkBox2.TabIndex = 2;
@@ -68,7 +75,7 @@
             // 
             // Dekompresja
             // 
-            this.Dekompresja.Location = new System.Drawing.Point(95, 13);
+            this.Dekompresja.Location = new System.Drawing.Point(92, 69);
             this.Dekompresja.Name = "Dekompresja";
             this.Dekompresja.Size = new System.Drawing.Size(81, 23);
             this.Dekompresja.TabIndex = 3;
@@ -78,19 +85,77 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(226, 13);
+            this.button2.Location = new System.Drawing.Point(335, 69);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
-            this.button2.Text = "szyfrowanie";
+            this.button2.Text = "deszyfracja";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ścieżka kompresja";
+            // 
+            // Plik
+            // 
+            this.Plik.Location = new System.Drawing.Point(115, 10);
+            this.Plik.Name = "Plik";
+            this.Plik.Size = new System.Drawing.Size(346, 20);
+            this.Plik.TabIndex = 6;
+            // 
+            // WybierzPlik
+            // 
+            this.WybierzPlik.Location = new System.Drawing.Point(469, 8);
+            this.WybierzPlik.Name = "WybierzPlik";
+            this.WybierzPlik.Size = new System.Drawing.Size(34, 23);
+            this.WybierzPlik.TabIndex = 7;
+            this.WybierzPlik.Text = "...";
+            this.WybierzPlik.UseVisualStyleBackColor = true;
+            this.WybierzPlik.Click += new System.EventHandler(this.WybierzPlik_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(219, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "szyfrowanie";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Ścieżka kompresja";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(346, 20);
+            this.textBox1.TabIndex = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(632, 213);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.WybierzPlik);
+            this.Controls.Add(this.Plik);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.Dekompresja);
             this.Controls.Add(this.checkBox2);
@@ -111,6 +176,12 @@
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.Button Dekompresja;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Plik;
+        private System.Windows.Forms.Button WybierzPlik;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- 2.KompresjaSzyfrowanie/Form1.cs
+++ 2.KompresjaSzyfrowanie/Form1.cs
@@ -2,8 +2,10 @@
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Windows.Forms;
+
 
 namespace _2.KompresjaSzyfrowanie
 {
@@ -14,50 +16,99 @@ namespace _2.KompresjaSzyfrowanie
         {
             InitializeComponent();
         }
-        Kompresja archiwum = new Kompresja();
-        Szyfrowanie szyfr = new Szyfrowanie();
-     //   public string directoryPath = @"C:\Users\Praktykant\Desktop\Nowy folder\Grafika\Rozdzial_1";
-
+        string sciezka = string.Empty;
+        Szyfrowanie EncryptFile = new Szyfrowanie();
+        Deszyfracja DecryptFile = new Deszyfracja();
+        string zipPath = @"C:\Users\Praktykant\Desktop\result.zip";
+        string extractPath = @"C:\Users\Praktykant\Desktop\extract";
+        
         private void button1_Click(object sender, EventArgs e)
         {
-/*            
-        DirectoryInfo directorySelected = new DirectoryInfo(directoryPath);
-            archiwum.Compress(directorySelected);*/
-            FolderBrowserDialog FolderSelect = new FolderBrowserDialog();
-            string sciezka = string.Empty;
-            if (FolderSelect.ShowDialog() == DialogResult.OK)
+            OpenFileDialog pplik = new OpenFileDialog() { Filter = "All files|*.*" };
+
+            string startPath = Plik.Text;
+
+
+            /*      DirectoryInfo directorySelected = new DirectoryInfo(directoryPath);
+                      archiwum.Compress(directorySelected);*/
+            /*   FolderBrowserDialog FolderSelect = new FolderBrowserDialog();
+               string sciezka = string.Empty;
+               if (FolderSelect.ShowDialog() == DialogResult.OK)
+               {
+                   sciezka = FolderSelect.SelectedPath;
+               }
+               DirectoryInfo Folderinfo = new DirectoryInfo(sciezka);
+               foreach (FileInfo plikInfo in Folderinfo.GetFiles())
+               {
+                ZipFile.CreateFromDirectory(sciezka, zipPath);
+            }*/
+            /* using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "All files|*.*" })
+             {
+                 if (ofd.ShowDialog() == DialogResult.OK)
+                     Plik.Text = ofd.FileName;
+                 ZipFile.CreateFromDirectory(Plik.Text, zipPath);
+             }*/
+
+           // ZipFile.CreateFromDirectory(sciezka, zipPath);
+
+            if (checkBox1.Checked)
             {
-                sciezka = FolderSelect.SelectedPath;
+                 ZipFile.CreateFromDirectory(sciezka, zipPath);
+                 Szyfrowanie.EncryptFile(zipPath, @"C:\Users\Praktykant\Desktop\extract.zip", "1234512345678976");
             }
-            DirectoryInfo Folderinfo = new DirectoryInfo(sciezka);
-            foreach (FileInfo plikInfo in Folderinfo.GetFiles())
+            else
             {
-               archiwum.Compress(plikInfo);
+                ZipFile.CreateFromDirectory(sciezka, zipPath);
             }
         }
 
         private void Dekompresja_Click(object sender, EventArgs e)
         {
-            FolderBrowserDialog FolderSelect = new FolderBrowserDialog();
-            string sciezka = string.Empty;
-            if (FolderSelect.ShowDialog() == DialogResult.OK)
-            {
-                sciezka = FolderSelect.SelectedPath;
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+              FolderBrowserDialog FolderSelect = new FolderBrowserDialog();
+              
+              if (FolderSelect.ShowDialog() == DialogResult.OK)
+              {
+                  sciezka = FolderSelect.SelectedPath;
 
-            }
-            DirectoryInfo Folderinfo = new DirectoryInfo(sciezka);
-            foreach (FileInfo plikInfo in Folderinfo.GetFiles())
-            {
-                archiwum.Decompress(plikInfo);
-            }
+              }
+              DirectoryInfo Folderinfo = new DirectoryInfo(sciezka);
+              foreach (FileInfo plikInfo in Folderinfo.GetFiles())
+              {
+                 // archiwum.Decompress(plikInfo);
+              }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-        
+            Deszyfracja.DecryptFile(@"C:\Users\Praktykant\Desktop\extract.zip", @"C:\Users\Praktykant\Desktop\extractss.zip", "1234512345678976");
         }
+
+        private void WybierzPlik_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FolderSelect = new FolderBrowserDialog();
             
-        
+            if (FolderSelect.ShowDialog() == DialogResult.OK)
+            {
+                sciezka = FolderSelect.SelectedPath;
+                Plik.Text = sciezka;
+            }
+            DirectoryInfo Folderinfo = new DirectoryInfo(sciezka);
+
+            /*using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "All files|*.*" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                    Plik.Text = ofd.FileName;
+            }*/
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Szyfrowanie.EncryptFile(@"C:\Users\Praktykant\Desktop\result.zip", @"C:\Users\Praktykant\Desktop\extract.zip", "1234512345678976");
+        }
     }
 }
 
